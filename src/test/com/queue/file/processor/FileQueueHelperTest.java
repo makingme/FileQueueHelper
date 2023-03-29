eeba03e3a8b95df8d696b69e7ab588376d9ed3b2
kibumkim <kibumkim@mz.co.kr>
2023-03-29 15:53:43 +0900

    junit & test code add

---
+++ src/test/com/queue/file/processor/FileQueueHelperTest.java
@@ -0,0 +1,91 @@
+package com.queue.file.processor;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FileQueueHelperTest {
+
+    @org.junit.Test
+    public void open() {
+    }
+
+    @org.junit.Test
+    public void close() {
+    }
+
+    @org.junit.Test
+    public void putData() {
+    }
+
+    @org.junit.Test
+    public void putDataList() {
+    }
+
+    @org.junit.Test
+    public void getData() {
+    }
+
+    @org.junit.Test
+    public void getDataList() {
+    }
+
+    @org.junit.Test
+    public void getDataCount() {
+    }
+
+    @org.junit.Test
+    public void closeCheckSum() {
+    }
+
+    @org.junit.Test
+    public void removePoorData() {
+    }
+
+    @org.junit.Test
+    public void clear() {
+    }
+
+    @org.junit.Test
+    public void isOk() {
+    }
+
+    @org.junit.Test
+    public void extractHint() {
+    }
+
+    @org.junit.Test
+    public void commit() {
+    }
+
+    @org.junit.Test
+    public void getSummaryMapData() {
+    }
+
+    @org.junit.Test
+    public void getQUEUE_NAME() {
+    }
+
+    @org.junit.Test
+    public void getQUEUE_PATH() {
+    }
+
+    @org.junit.Test
+    public void getOpenTime() {
+    }
+
+    @org.junit.Test
+    public void getLastInTime() {
+    }
+
+    @org.junit.Test
+    public void getLastOutTime() {
+    }
+
+    @org.junit.Test
+    public void getInputCnt() {
+    }
+
+    @org.junit.Test
+    public void getOutputCnt() {
+    }
+}
